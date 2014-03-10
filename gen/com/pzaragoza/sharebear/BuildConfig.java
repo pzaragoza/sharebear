f24335596cf6b5d16d91b04bd083f675a11fd100
Pilar Zaragoza <pia@172-29-3-168.dynapool.nyu.edu>
2014-03-10 15:55:01 -0400

    first commit

---
+++ gen/com/pzaragoza/sharebear/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.pzaragoza.sharebear;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/pzaragoza/sharebear/MainActivity.java
@@ -0,0 +1,45 @@
+package com.pzaragoza.sharebear;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener {
+	Button button1; 
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		button1 = (Button) this.findViewById(R.id.button1);
+		button1.setOnClickListener(this);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+
+		Intent intent=new Intent(android.content.Intent.ACTION_SEND);
+		intent.setType("text/plain");
+		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+
+		// Add data to the intent, the receiving app will decide what to do with it.
+		intent.putExtra(Intent.EXTRA_SUBJECT, "Some Subject Line");
+		intent.putExtra(Intent.EXTRA_TEXT, "Body of the message, woot!");
+		
+		startActivity(intent);
+	}
+
+}
